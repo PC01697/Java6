2ec7aa5c17900a1b709ac0b407cd5ab3b20c1b20
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-07-15 18:14:02 +0700

    khoa join

---
+++ Normal_J6/src/main/java/pc01815/Normal_J6/Controller/testController.java
@@ -0,0 +1,7 @@
+package pc01815.Normal_J6.Controller;
+
+public class testController {
+		public static void main(String[] args) {
+			System.err.println("test");
+		}
+}
---
+++ Normal_J6/src/test/java/pc01815/Normal_J6/test.java
@@ -0,0 +1,8 @@
+package pc01815.Normal_J6;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.err.println("git project run");
+	}
+}
