35a38153f6633dec2d5a7a9a17b39a20fdac26fc
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-07-17 18:03:19 +0700

    update entity and application properties

--- Normal_J6/src/main/java/pc01815/Normal_J6/Controller/testController.java
+++ Normal_J6/src/main/java/pc01815/Normal_J6/Controller/testController.java
@@ -1,7 +1,25 @@
 package pc01815.Normal_J6.Controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pc01815.Normal_J6.Entity.Accounts;
+import pc01815.Normal_J6.Repository.AccountsRepository;
+
+@RestController
 public class testController {
-		public static void main(String[] args) {
-			System.err.println("test");
-		}
+
+	@Autowired
+	AccountsRepository accRepo;
+	
+	@RequestMapping("/test")
+	public List<Accounts> getAll(){
+		List<Accounts> list = accRepo.findAll();
+		
+		return list.stream().collect(Collectors.toList());
+	}
 }
