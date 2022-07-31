b90553dc7c5e4e2897eee9aae6749cba5435dcf8
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-07-31 19:46:12 +0700

    update components admin page

---
+++ Normal_J6/src/main/resources/static/adminAsset/app.js
@@ -0,0 +1,5 @@
+app.controller("header-footer", function ($scope) {
+  $scope.navbar = "../adminAsset/components/navbar.html";
+  $scope.aside = "../adminAsset/components/aside.html"
+   $scope.footer = "../adminAsset/components/footer.html";
+});
