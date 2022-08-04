9229b2ac5ae2186b0876474f38da92ce0c7b5624
HoLy <HoLy@26.240.170.117>
2022-08-04 19:25:43 +0700

    HoLy update

--- Normal_J6/src/main/java/pc01815/Normal_J6/Controller/NguoiDung/ChiTiet.java
+++ Normal_J6/src/main/java/pc01815/Normal_J6/Controller/NguoiDung/ChiTiet.java
@@ -1,13 +1,30 @@
 package pc01815.Normal_J6.Controller.NguoiDung;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pc01815.Normal_J6.Entity.Category;
+import pc01815.Normal_J6.Entity.Products;
+import pc01815.Normal_J6.Repository.CategoryRepository;
+import pc01815.Normal_J6.Repository.ProductsRepository;
 @Controller
 public class ChiTiet {
+	@Autowired
+	ProductsRepository productDao;
+	@Autowired
+CategoryRepository categoryDao;
 	@RequestMapping("/chitiet/{id}")
-	public String form(Model model) {
+	public String form(Model model,@PathVariable("id") Integer id) {
+		List<Category> item = categoryDao.findAll();
+		model.addAttribute("item", item);
 		
+        Products product = productDao.findById(id).get();
+        model.addAttribute("product", product);
 		return "NguoiDung/chitiet";
 		
 	}
