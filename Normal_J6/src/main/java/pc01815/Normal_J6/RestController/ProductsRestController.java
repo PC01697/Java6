db8bb8bd6d5d6c7f3fff28ef593167a7a4024b38
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-08-07 18:13:27 +0700

    upload defaul img and fileupload ultil

--- Normal_J6/src/main/java/pc01815/Normal_J6/RestController/ProductsRestController.java
+++ Normal_J6/src/main/java/pc01815/Normal_J6/RestController/ProductsRestController.java
@@ -1,9 +1,11 @@
 package pc01815.Normal_J6.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,10 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import pc01815.Normal_J6.Entity.Products;
 import pc01815.Normal_J6.Services.ProductsService;
+import pc01815.Normal_J6.Util.FileUploadUtil;
 
 @RestController
 @RequestMapping("api")
@@ -29,7 +32,10 @@ public class ProductsRestController {
 	@Autowired
 	ProductsService productsService;
 	
-	@GetMapping(value = "/products", produces = "application/json;charset=UTF-8")
+	@Autowired
+	ServletContext app;
+	
+	@GetMapping(value = "/products", produces = "application/json")
 	public ResponseEntity<List<Products>> findAllCategory(
 			@RequestParam("page") Optional<Integer> page,
 			@RequestParam("entry") Optional<Integer> entry,
@@ -51,6 +57,15 @@ public class ProductsRestController {
 	@PostMapping(value = "/products")
 	public ResponseEntity<Products> saveCategory(@RequestBody Products products){
 		return new ResponseEntity<Products>(productsService.saveProductsService(products),HttpStatus.CREATED);
+	}
+	
+	@PostMapping(value = "/test")
+	public String test(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+		
+		FileUploadUtil test = new FileUploadUtil();
+		test.saveFile(file, app);
+		
+		return "ok";
 	}
 	
 	
--- Normal_J6/src/main/java/pc01815/Normal_J6/Util/FileUploadUtil.java
+++ Normal_J6/src/main/java/pc01815/Normal_J6/Util/FileUploadUtil.java
@@ -6,13 +6,14 @@ import java.util.Random;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
 	
-	private static String generateRandomPassword(int len) {
+	private static String generateRandomName(int len) {
 		String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk"
           +"lmnopqrstuvwxyz!@#$%&";
 		Random rnd = new Random();
@@ -22,15 +23,24 @@ public class FileUploadUtil {
 		return sb.toString();
 	}
 	
-	public void saveFile(MultipartFile file, ServletContext test) throws IllegalStateException, IOException {
+	public void saveFile(MultipartFile file, ServletContext app) throws IllegalStateException, IOException {
 		if(!file.isEmpty()) {
 			String getFile = file.getOriginalFilename();
 			String getExtension = FilenameUtils.getExtension(getFile);
-			String filenamnRandom = generateRandomPassword(20);
+			String filenamnRandom = generateRandomName(20);
 			String filename = filenamnRandom + "." + getExtension;
 			
-			File saveFile = new File(test.getRealPath("/docs/" + filename));
+			File saveFile = new File(app.getRealPath("/imgProducts/" + filename));
 			file.transferTo(saveFile);
+		}else {
+			File getDefaulImg = new File(app.getRealPath("/defaulImgProducts/default.png"));
+			String getExtenstionOfDefault = FilenameUtils.getExtension(getDefaulImg.getName());
+			String rdFileName = generateRandomName(20);
+			String fileDefault = rdFileName + "." + getExtenstionOfDefault;
+			File saveFile = new File(app.getRealPath("/imgProducts/" + fileDefault));
+			FileUtils.copyFile(getDefaulImg, saveFile);
+//			System.err.println(getExtenstionOfDefault);
+//			System.err.println("đang null");
 		}
 	}
 }
