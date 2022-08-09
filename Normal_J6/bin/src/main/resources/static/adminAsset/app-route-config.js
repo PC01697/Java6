73f60366839177fd4058bb402237b2a062057ff0
HoLy <HoLy@DESKTOP-C2KB8R8>
2022-08-09 17:37:27 +0700

    KO CÓ J

---
+++ Normal_J6/bin/src/main/resources/static/adminAsset/app-route-config.js
@@ -0,0 +1,34 @@
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  var productState = {
+    name: "products",
+    url: "/products",
+    templateUrl: "../adminAsset/components/products.html",
+    data: {
+      label: "Products",
+    },
+  };
+
+  var accountState = {
+    name: "accounts",
+    url: "/accounts",
+    templateUrl: "../adminAsset/components/accounts.html",
+    controller: "accountsCrt",
+    
+  };
+
+  var categoryState = {
+    name: "categories",
+    url: "/categories",
+    templateUrl: "../adminAsset/components/categories.html",
+    controller: "categoryCrt"
+  };
+
+  $stateProvider.state(productState);
+  $stateProvider.state(accountState);
+  $stateProvider.state(categoryState);
+  $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+  });
+});
